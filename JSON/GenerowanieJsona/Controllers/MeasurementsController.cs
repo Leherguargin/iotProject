ab9c34eb6c223a1ab6c47ef7c063ec5878ea33d1
Matixz <matixz.kom@gmail.com>
2018-11-04 01:33:03 +0100

    JSON

---
+++ JSON/GenerowanieJsona/Controllers/MeasurementsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GenerowanieJsona.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Measurements")]
+    [ApiController]
+    public class MeasurementsController : ControllerBase
+    {
+        private static List<Models.Measurements> lista = new List<Models.Measurements>(new[]{
+                new Models.Measurements(1,1,433.34,DateTime.Now),
+                new Models.Measurements(2,2,54.7,DateTime.Now),
+            });
+        [HttpGet]
+        public List<Models.Measurements> Get()
+        {
+            return lista;
+        }
+        [HttpGet("{SensorID}")]
+        public Models.Measurements Get(int id)
+        {
+            return lista.SingleOrDefault(lista => lista.SensorID == id);
+        }
+    }
+}
---
+++ JSON/GenerowanieJsona/Models/Measurements.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GenerowanieJsona.Models
+{
+    public class Measurements
+    {
+        public int MeasurementsID { get; set; }
+        public int SensorID { get; set; }
+        public double Value { get; set; }
+        public DateTime Time { get; set; }
+
+        public Measurements(int measurementsID, int sensorID, double value, DateTime time)
+        {
+            MeasurementsID = measurementsID;
+            SensorID = sensorID;
+            Value = value;
+            Time = time;
+        }
+    }
+}
