7dc076ea40df26a9eae4104ec942735e5e7175b9
Leherguargin <maciejleszczynski1997@gmail.com>
2018-12-07 01:28:28 +0100

    sensor

---
+++ LoraStore/LoraStore/Models/Sensor.cs
@@ -0,0 +1,10 @@
+﻿namespace LoraStore.Models
+{
+    public class Sensor
+    {
+        public int SensorId { get; set; }
+        public string SensorName { get; set; }
+        public string SensorPassword { get; set; }
+        public string SensorDescription { get; set; }
+    }
+}
