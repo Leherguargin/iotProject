bba0eea0d3aed3b1b97b41a51f93fcd0466e1fb4
Leherguargin <maciejleszczynski1997@gmail.com>
2018-12-08 09:13:00 +0100

    usnalem xd

---
+++ LoraStore/LoraStore/Controllers/SensorsController.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using LoraStore.Data;
+using LoraStore.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace LoraStore.Controllers
+{
+    [Authorize]
+    public class SensorsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SensorsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Sensors
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Sensors.ToListAsync());
+        }
+
+        // GET: Sensors/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var sensor = await _context.Sensors
+                .FirstOrDefaultAsync(m => m.SensorId == id);
+            if (sensor == null)
+            {
+                return NotFound();
+            }
+
+            return View(sensor);
+        }
+
+        // GET: Sensors/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Sensors/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("SensorId,SensorName,SensorPassword,SensorDescription")] Sensor sensor)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(sensor);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(sensor);
+        }
+
+        // GET: Sensors/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var sensor = await _context.Sensors.FindAsync(id);
+            if (sensor == null)
+            {
+                return NotFound();
+            }
+            return View(sensor);
+        }
+
+        // POST: Sensors/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("SensorId,SensorName,SensorPassword,SensorDescription")] Sensor sensor)
+        {
+            if (id != sensor.SensorId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(sensor);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SensorExists(sensor.SensorId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(sensor);
+        }
+
+        // GET: Sensors/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var sensor = await _context.Sensors
+                .FirstOrDefaultAsync(m => m.SensorId == id);
+            if (sensor == null)
+            {
+                return NotFound();
+            }
+
+            return View(sensor);
+        }
+
+        // POST: Sensors/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var sensor = await _context.Sensors.FindAsync(id);
+            _context.Sensors.Remove(sensor);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SensorExists(int id)
+        {
+            return _context.Sensors.Any(e => e.SensorId == id);
+        }
+    }
+}
